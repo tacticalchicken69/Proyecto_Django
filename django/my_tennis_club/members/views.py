42006e1a1db2ac0142d23af350df0c3f571b1e0a
Andres Gonzalez <andres_gonz4lez@outlook.com>
2025-11-24 11:46:14 -0500

    nuevos cambios saraparapampin

--- django/my_tennis_club/members/views.py
+++ django/my_tennis_club/members/views.py
@@ -26,8 +26,26 @@ def main(request):
 def testing(request):
   template = loader.get_template('template.html')
   mymembers = Member.objects.all().values()
+  column_firstname = Member.objects.values_list('firstname')
+  records_uribe = Member.objects.filter(column_firstname='uribe').values()
+  record_uribe_westcol = Member.objects.filter(firstname='uribe', id=2).values()
+  record_OR_andres_maria= Member.objects. filter(Q(firstname= 'Andres') | Q(firstname='Maria'))
+  record_like_start_L = Member.objects. filter(firstname_istartswith='L'). values()
+  record_like_ends_s= Member objects. filter(firstname_iendswith='s'). values ()
+  Grecord_like_contains_ez= Member. objects filter(lastname_icontains='ez'). values()
+  order_by_asc= Member. objects.all() order_by'firstname'). values ()
+  order_by_des= Member objects.all). order _by('-firstname"). values ()
   context = {
     'fruits': ['Apple', 'Banana', 'Cherry'], 
     'mymembers': mymembers,  
+    'column_firstname': column_firstname,  
+    'records_andres': records_andres,
+    'record_andres_sandoval': record_AND_andres_sandoval,
+    "record_OR_andres_maria": record_oR_andres_maria,
+    'record_like_start_L': record_like_start_L,
+    'record_like_ends_s': record_like_ends_s,
+    'record_like_contains_ez': record_like_contains_ez,
+    'order_by_asc': order_by_asc,
+    'order_by_desc": order_by_desc,
   }
-  return HttpResponse(template.render(context, request))
+  return HttpResponse(template.render(context, requrest))
